8e0b874fe320704ee2fb6a5cbdebfea2ef54b672
YemsAvilaGalvez <avilagalvezraul@gmail.com>
2025-01-11 20:42:10 -0500

    index

--- adm/views/index.php
+++ adm/views/index.php
@@ -286,9 +286,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </footer>
     </div>
     <!-- ./wrapper -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+    
     <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- datatable -->
     <script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
     <script src="assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
     <script src="assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
@@ -301,9 +307,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
     <script src="assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
     <script src="assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-    <!-- jQuery -->
-    <!-- Bootstrap 4 -->
-    <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- Select2 -->
     <script src="assets/plugins/select2/js/select2.full.min.js"></script>
     <!-- AdminLTE App -->
